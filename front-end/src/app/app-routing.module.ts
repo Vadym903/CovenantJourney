2671092e6f27b2eb34187c751604f1f50ee3fbe4
VadimVvs <vadimzno18@gmail.com>
2022-04-03 11:32:24 +0300

    started to work with UI part and added home/nav-bar components

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from "./components/home/home.component";
+import { MapComponent } from "./components/map/map.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: MapComponent}
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,21 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
 
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { MarkerCardComponent } from './components/marker-card/marker-card.component';
+import { MarkerCardComponent } from './components/map/marker-card/marker-card.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MapComponent,
+    MarkerCardComponent,
     HomeComponent,
-    MarkerCardComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LeafletModule
+    LeafletModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
