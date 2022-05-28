5afd8cc164cc0eb4041b163f64ce1d570a0f882d
VadimVvs <vadimzno18@gmail.com>
2022-05-28 13:11:25 +0300

    Added edit mode for user's profile

--- front-end/src/app/components/user-info/user-info.component.ts
+++ front-end/src/app/components/user-info/user-info.component.ts
@@ -3,6 +3,9 @@ import { ActivatedRoute } from "@angular/router";
 import { User } from "../../_models/user.model";
 import { UserService } from "../../services/user.service";
 import { DomSanitizer } from "@angular/platform-browser";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ApartmentService } from "../../services/apartment.service";
+import { Apartment } from "../../_models/apartment.model";
 
 @Component({
 	selector: 'app-user-info',
@@ -13,9 +16,14 @@ export class UserInfoComponent implements OnInit {
 
 	user: User;
 	userDescriptionHtml;
-
+	isEdit = false;
+	userForm: FormGroup = new FormGroup({});
+	apartments: Apartment[] = [];
+	
 	constructor(private activatedRoute: ActivatedRoute,
 				private userService: UserService,
+				private apartmentService: ApartmentService,
+				private fb: FormBuilder,
 				private sanitizer: DomSanitizer) {
 	}
 
@@ -25,7 +33,35 @@ export class UserInfoComponent implements OnInit {
 			this.user = user;
 			this.userDescriptionHtml = this.sanitizer.bypassSecurityTrustHtml(user.description);
 		});
+	}
 
+	editMode(): void {
+		this.isEdit = true;
+		this.initUserForm();
+	}
+
+	initUserForm(): void {
+		this.userForm = this.fb.group({
+			fullName: this.fb.control(this.user.fullName),
+			description: this.fb.control(this.user.description)
+		});
+	}
+
+	cancel(): void {
+		this.initUserForm();
+		this.isEdit = false;
+	}
+
+	save(): void {
+		this.userService.updateCurrentUser(this.userForm.value).subscribe(user => {
+			this.initUser(user);
+			this.cancel();
+		});
+	}
+
+	initUser(user: User): void {
+		this.user = user;
+		this.userDescriptionHtml = this.sanitizer.bypassSecurityTrustHtml(user.description);
 	}
 
 }
